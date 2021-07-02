c996cc0d5fa26ede5f001c26195a958fb5d6c3a4
Daniel Fourie <daniel.fourie@johnlewis.co.uk>
2021-07-02 10:17:37 +0200

    cleanup

--- nvim/lua/ice/init.lua
+++ nvim/lua/ice/init.lua
@@ -1,7 +1,6 @@
 require("ice.telescope")
 require("ice.compe-config")
 require("ice.treesitter")
-require("ice.treesitter")
 require("ice.lualine")
 require("ice.lspinstall")
 require("ice.lspsaga")
