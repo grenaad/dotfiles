5b9ad414f7f9126b1f10d6ea9c5d127bc9eb78a7
grenaad <dan4rie@gmail.com>
2022-10-05 14:43:43 +0200

    updated lvim, also required changing config file

--- configs/lvim/.config/lvim/config.lua
+++ configs/lvim/.config/lvim/config.lua
@@ -1,11 +1,10 @@
-
 -- ########### General ###########
 
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+lvim.builtin.alpha.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+-- lvim.builtin.nvimtree.show_icons.git = 0
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "onedarker"
@@ -33,70 +32,70 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- ########### Additional Plugins ###########
 
 lvim.plugins = {
-    {"folke/trouble.nvim",
-      cmd = "TroubleToggle",
-    },
-    {"udalov/kotlin-vim"}, -- Syntax for kotlin
-    { -- brew install glow
-     "npxbr/glow.nvim",
-     ft = {"markdown"} -- markdown previewer
-    },
-    {"sindrets/diffview.nvim",
-      event = "BufRead",
-    },
-    {'TimUntersberger/neogit',
-      config = function ()
-        require("neogit").setup ({
-          disable_commit_confirmation = true,
-          integrations = { diffview = true }
-        })
-      end,
-    },
-    {"PhilT/vim-fsharp"}, -- Fsharp syntax and indenting
-    {"ruifm/gitlinker.nvim",
-      event = "BufRead",
-      config = function()
-        require("gitlinker").setup {
-            opts = {
-              -- remote = 'github', -- force the use of a specific remote
-              add_current_line_on_normal_mode = true, -- adds current line nr in the url for normal mode
-              action_callback = require("gitlinker.actions").copy_to_clipboard, -- callback for what to do with the url
-              print_url = true, -- print the url after performing the action
-              mappings = "<leader>gy", -- mapping to call url generation
-            },
-          }
-      end,
-      requires = "nvim-lua/plenary.nvim",
-    },
-    {"kevinhwang91/nvim-bqf",
-      event = { "BufRead", "BufNew" },
-      config = function()
-        require("bqf").setup({
-          auto_enable = true,
-          preview = {
-            win_height = 12,
-            win_vheight = 12,
-            delay_syntax = 80,
-            border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
-          },
-          func_map = {
-            vsplit = "",
-            ptogglemode = "z,",
-            stoggleup = "",
-          },
-          filter = {
-            fzf = {
-              action_for = { ["ctrl-s"] = "split" },
-              extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
-            },
-          },
-         })
-      end,
-      },
-      {"ThePrimeagen/harpoon"},
-      {"meain/vim-package-info",
-        run ="npm install"
+  { "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
+  { "udalov/kotlin-vim" }, -- Syntax for kotlin
+  { -- brew install glow
+    "npxbr/glow.nvim",
+    ft = { "markdown" } -- markdown previewer
+  },
+  { "sindrets/diffview.nvim",
+    event = "BufRead",
+  },
+  { 'TimUntersberger/neogit',
+    config = function()
+      require("neogit").setup({
+        disable_commit_confirmation = true,
+        integrations = { diffview = true }
+      })
+    end,
+  },
+  { "PhilT/vim-fsharp" }, -- Fsharp syntax and indenting
+  { "ruifm/gitlinker.nvim",
+    event = "BufRead",
+    config = function()
+      require("gitlinker").setup {
+        opts = {
+          -- remote = 'github', -- force the use of a specific remote
+          add_current_line_on_normal_mode = true, -- adds current line nr in the url for normal mode
+          action_callback = require("gitlinker.actions").copy_to_clipboard, -- callback for what to do with the url
+          print_url = true, -- print the url after performing the action
+          mappings = "<leader>gy", -- mapping to call url generation
+        },
       }
+    end,
+    requires = "nvim-lua/plenary.nvim",
+  },
+  { "kevinhwang91/nvim-bqf",
+    event = { "BufRead", "BufNew" },
+    config = function()
+      require("bqf").setup({
+        auto_enable = true,
+        preview = {
+          win_height = 12,
+          win_vheight = 12,
+          delay_syntax = 80,
+          border_chars = { "┃", "┃", "━", "━", "┏", "┓", "┗", "┛", "█" },
+        },
+        func_map = {
+          vsplit = "",
+          ptogglemode = "z,",
+          stoggleup = "",
+        },
+        filter = {
+          fzf = {
+            action_for = { ["ctrl-s"] = "split" },
+            extra_opts = { "--bind", "ctrl-o:toggle-all", "--prompt", "> " },
+          },
+        },
+      })
+    end,
+  },
+  { "ThePrimeagen/harpoon" },
+  { "meain/vim-package-info",
+    run = "npm install"
+  }
 }
 
 vim.opt.colorcolumn = "150"
@@ -116,17 +115,17 @@ vim.cmd("set textwidth=150")
 
 -- Fsharp LSP
 local util = require('lspconfig/util')
-require'lspconfig'.fsautocomplete.setup{
-    cmd = {'fsautocomplete', '--background-service-enabled' },
-    root_dir = util.root_pattern('*.sln', '.git'),
+require 'lspconfig'.fsautocomplete.setup {
+  cmd = { 'fsautocomplete', '--background-service-enabled' },
+  root_dir = util.root_pattern('*.sln', '.git'),
 }
 
-require'lspconfig'.dartls.setup{
-  settings ={
-  dart = {
-    lineLength = 150,
-    completeFunctionCalls = true,
-    showTodos = true
+require 'lspconfig'.dartls.setup {
+  settings = {
+    dart = {
+      lineLength = 150,
+      completeFunctionCalls = true,
+      showTodos = true
     }
   }
 }
@@ -137,8 +136,8 @@ require'lspconfig'.dartls.setup{
 
 -- ## Builtin ##
 
-    -- Appending to root dir pattern
-lvim.builtin.project.patterns[#lvim.builtin.project.patterns+1]="*.sln"
+-- Appending to root dir pattern
+lvim.builtin.project.patterns[#lvim.builtin.project.patterns + 1] = "*.sln"
 
 -- LSP, was copied from lvim configs, these mappings are not loaded with lvim for some reason
 lvim.keys.normal_mode["K"]  = ":lua vim.lsp.buf.hover()<cr>"
@@ -153,7 +152,8 @@ lvim.keys.normal_mode["gl"] = ":lua require'lvim.lsp.handlers'.show_line_diagnos
 -- ## Whichkey ##
 
 -- Git Linker
-lvim.builtin.which_key.mappings.g.y = { "<cmd>lua require('gitlinker').get_buf_range_url('n')<cr>", "Copy permalink to clipboard" }
+lvim.builtin.which_key.mappings.g.y = { "<cmd>lua require('gitlinker').get_buf_range_url('n')<cr>",
+  "Copy permalink to clipboard" }
 
 -- Neogit and Diffview
 lvim.builtin.which_key.mappings.g.w = { "<cmd>Neogit<cr>", "Status Window" }
@@ -164,9 +164,11 @@ lvim.builtin.which_key.mappings.g.P = lvim.builtin.which_key.mappings.g.p -- swi
 lvim.builtin.which_key.mappings.g.p = { "<cmd>Neogit push<cr>", "Push" }
 
 -- Telescope
-lvim.builtin.which_key.mappings.s.d = { "<cmd>lua require('telescope.builtin').git_files({hidden=true, prompt_title='< VimRC >', cwd='$HOME/projects/dotfiles/',})<cr>", "Search dotfiles" }
+lvim.builtin.which_key.mappings.s.d = { "<cmd>lua require('telescope.builtin').git_files({hidden=true, prompt_title='< VimRC >', cwd='$HOME/projects/dotfiles/',})<cr>",
+  "Search dotfiles" }
 lvim.builtin.which_key.mappings["F"] = lvim.builtin.which_key.mappings["f"]
-lvim.builtin.which_key.mappings["f"] = { "<cmd>lua require('telescope.builtin').git_files({hidden=true})<cr>", "Find all files" }
+lvim.builtin.which_key.mappings["f"] = { "<cmd>lua require('telescope.builtin').git_files({hidden=true})<cr>",
+  "Find all files" }
 
 -- Trouble
 -- jump to the previous item, skipping the groups
@@ -175,8 +177,8 @@ lvim.keys.normal_mode["<C-]>"] = ":lua require('trouble').previous({skip_groups 
 lvim.keys.normal_mode["<C-[>"] = ":lua require('trouble').next({skip_groups = true, jump = true})<cr>"
 
 lvim.builtin.which_key.mappings.t = { -- Trouble, jump to lsp error diagnostics
-    name = "Diagnostics",
-    t = { "<cmd>TroubleToggle<cr>", "Trouble" },
+  name = "Diagnostics",
+  t = { "<cmd>TroubleToggle<cr>", "Trouble" },
 }
 
 -- Markdown
@@ -184,9 +186,9 @@ lvim.builtin.which_key.mappings.m = { "<cmd>Glow<cr>", "View Markdown files" }
 
 -- Harpoon
 lvim.builtin.which_key.mappings.n = {
-    name = "Harpoon",
-    m = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Quick Menu" },
-    a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Add File" },
+  name = "Harpoon",
+  m = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Quick Menu" },
+  a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Add File" },
 }
 lvim.keys.normal_mode["<C-n>"] = ":lua require('harpoon.ui').nav_next()<cr>"
 lvim.keys.normal_mode["<C-p>"] = ":lua require('harpoon.ui').nav_prev()<cr>"
@@ -203,4 +205,3 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>" -- save file
 lvim.keys.normal_mode["<C-z>"] = ":q<cr>" -- override suspend of vim, rather just quit
 -- vim.cmd("nnoremap gf :edit <cfile><cr>") -- Allows gf to open non existing files
-
