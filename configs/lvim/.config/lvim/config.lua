756917f963bd0ad6134dd35ca4193fd95422bb7f
grenaad <dan4rie@gmail.com>
2023-12-15 11:30:43 +0200

    updated to include go binaries and fixed js file to use typescript lsp

--- configs/lvim/.config/lvim/config.lua
+++ configs/lvim/.config/lvim/config.lua
@@ -139,6 +139,8 @@ require 'lspconfig'.dartls.setup {
 -- autocommand set Metatrader file types
 vim.cmd("au BufNewFile,BufRead *.mqh,*.mq4,*.mq5 set filetype=cpp")
 
+vim.cmd("au BufNewFile,BufRead *.js set filetype=typescript")
+
 -- dotfiles file types
 vim.cmd("au BufNewFile,BufRead *.functions,*.aliases set filetype=bash")
 -- ########### Configure plugins ###########
