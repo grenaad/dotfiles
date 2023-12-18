d262cc9a7bd33a3a2efb80b5b8bf686cd7a9373f
grenaad <dan4rie@gmail.com>
2023-12-18 09:03:15 +0200

    added temp vscode extensions

---
+++ vscode_ext.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+code  --list-extensions --show-versions 
+# code --install-extension asvetliakov.vscode-neovim
+
+# a-h.templ@0.0.17
+# asvetliakov.vscode-neovim@1.4.0
+# bradlc.vscode-tailwindcss@0.10.3
+# eamodio.gitlens@14.6.1
+# esbenp.prettier-vscode@10.1.0
+# golang.go@0.40.0
+# humao.rest-client@0.25.1
+# James-Yu.latex-workshop@9.15.8
+# jdinhlife.gruvbox@1.17.0
+# ms-azuretools.vscode-docker@1.28.0
+# ms-vscode-remote.remote-containers@0.327.0
+# ms-vscode.cpptools@1.18.5
+# ms-vscode.cpptools-extension-pack@1.3.0
+# ms-vscode.cpptools-themes@2.0.0
+# otovo-oss.htmx-tags@0.0.8
+# p1c2u.docker-compose@0.5.1
+# streetsidesoftware.code-spell-checker@3.0.1
+# VSpaceCode.whichkey@0.11.3
+#
