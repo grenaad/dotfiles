d9e98da40cc06eaa0bb3b6329e9fa3dc09da8a27
grenaad <dan4rie@gmail.com>
2024-09-23 10:22:19 +0200

    updated python lsp

--- configs/nvim/.config/nvim/lua/polish.lua
+++ configs/nvim/.config/nvim/lua/polish.lua
@@ -17,15 +17,6 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
--- require("lspconfig").dartls.setup({
---   settings = {
---     dart = {
---       completeFunctionCalls = true,
---       showTodos = true
---     },
---    },
--- })
-
 -- Set up custom filetypes
 vim.filetype.add {
   extension = {
@@ -38,3 +29,38 @@ vim.filetype.add {
     ["~/%.config/foo/.*"] = "fooscript",
   },
 }
+
+-- Python setup
+-- pyright and ruff 
+require('lspconfig').pyright.setup {
+  settings = {
+    pyright = {
+      -- Using Ruff's import organizer
+      disableOrganizeImports = true,
+      -- disableLanguageServices = false,
+      -- disableTaggedHints  = false,
+    },
+    python = {
+      analysis = {
+        -- Ignore all files for analysis to exclusively use Ruff for linting
+        ignore = { '*' },
+      },
+    },
+  },
+}
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client == nil then
+      return
+    end
+    if client.name == 'ruff' then
+      -- Disable hover in favor of Pyright
+      client.server_capabilities.hoverProvider = false
+    end
+  end,
+  desc = 'LSP: Disable hover capability from Ruff',
+})
+
