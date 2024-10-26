432e59c8778780b1c141daf8a3f7d9a365d7fb8f
grenaad <dan4rie@gmail.com>
2024-10-26 13:10:27 +0200

    added tailwindcss

--- configs/nvim/.config/nvim/lua/plugins/mason.lua
+++ configs/nvim/.config/nvim/lua/plugins/mason.lua
@@ -12,9 +12,11 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
+        "gopls",
         "templ",
         "html-lsp",
         "htmx-lsp",
+        "tailwindcss-language-server",
         -- add more arguments for adding more language servers
       })
     end,
